8b6fb96987585cb2be7b53639aa6ef822def7c53
Abhishek Juneja <abhishek.juneja@bizongo.com>
2019-07-08 18:21:53 +0530

    adding element to the existing list

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TopicService {
 	
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "SF Desc"),
 			new Topic("rails", "Rails Framework", "Rails Desc"),
 			new Topic("django", "Django Framework", "Django Desc")
-			);
+			));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -23,6 +24,10 @@ public class TopicService {
 	public Topic getTopic(String id) {
 //		iterate over a list and run a map on the id
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
